a618da85da11df58f61b6e1ad4b9fbcdb00c71a0
SRay-2020 <stephen.r.ray@gmail.com>
2021-04-30 17:48:38 +0000

    Begin debug for email.js - remove return empty fields

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -2,28 +2,28 @@
 // EmailJS email service
 // Code Institute Lessons
 // Fucntion calls on emailjs to send mail and resend in template to author
-let emailjs;
+// let emailjs;
 function sendMail(contactForm) {
     emailjs.send("service_motdhpp", "template_kjnqbzq", {
         "from_name": contactForm.name.value,
-        "feedback_request": contactForm.feedbacksummary.value,
         "from_email": contactForm.email.value,
+        "feedback_request": contactForm.feedbacksummary.value
     })
 
 
-        .then(function (response) {
+        .then(
+            function(response) {
 
-            // Stack Overflow lesson - return empty fields
-            console.log("SUCCESS!", response);
-            document.getElementById('fullname').value = '';
-            document.getElementById('email').value = '';
-            // Returns a comment to user in feedback field to confirm email has sent 
-            document.getElementById('feedback-summary').value = 'We have received your email. Thank you for your feedback :)';
-        },
-
-            function (error) {
+                // Stack Overflow lesson - return empty fields
+                console.log("SUCCESS!", response);
+                // document.getElementById('fullname').value = '';
+                // document.getElementById('email').value = '';
+                // // Returns a comment to user in feedback field to confirm email has sent 
+                // document.getElementById('feedback-summary').value = 'We have received your email. Thank you for your feedback :)';
+            },
+            function(error) {
                 console.log("FAILED", error);
             });
 
-    return false;
+    // return false;
 }
