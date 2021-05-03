c4a21bf2cc26b340bbb5e863cf3c1fdd5e257e6c
SRay-2020 <stephen.r.ray@gmail.com>
2021-05-03 19:32:20 +0000

    Resolve git conflicts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,8 @@
-<<<<<<< HEAD
 /*jshint esversion: 6 */
 // Ref - Code Institute (Course content), PortExe (Video), Medium (Article), WebDev (Video), Invention Tricks (Video) - see README.md
 
 // Contains template of game object(cards array, timer and turn counter)
 class LanguageGame {
-=======
-// Ref - Code Institute (Course content), PortExe (Video), Medium (Article), WebDev (Video), Invention Tricks (Video) - see README.md
-// Audio Controler 
-
-class AudioController {
-    constructor() {
-        this.bgMusic = new Audio('assets/music/BackMusic.wav')
-        this.flipSound = new Audio('assets/music/newflipper.wav')
-        this.matchSound = new Audio('assets/music/matchSound.wav')
-        this.victorySound = new Audio('assets/music/winSound.wav')
-        this.gameOverSound = new Audio('assets/music/gameoverSound.wav')
-        this.bgMusic.volume = 0.5;
-        this.bgMusic.loop = true;
-    }
-    // Music Functions
-    startMusic() {
-        this.bgMusic.play();
-    }
-    stopMusic() {
-        this.bgMusic.pause();
-        this.bgMusic.currentTime = 0;
-    }
-
-    flip() {
-        this.flipSound.play();
-    }
-    match() {
-        this.matchSound.play();
-    }
-    victory() {
-        this.stopMusic();
-        this.victorySound.play();
-    }
-    gameOver() {
-        this.stopMusic();
-        this.gameOverSound.play();
-    }
-}
-
-// 
-class MixOrMatch {
->>>>>>> 8484efa (Clean code and comments for JS)
     constructor(totalTime, cards) {
         this.cardsArray = cards;
         this.totalTime = totalTime;
@@ -54,10 +11,7 @@ class MixOrMatch {
         this.ticker = document.getElementById('turn-amount');
     }
     //  Start Game Function
-<<<<<<< HEAD
     // Resets cards to check to zero, total clicks to 0 & matched cards array back to 0
-=======
->>>>>>> 8484efa (Clean code and comments for JS)
     startGame() {
         this.cardToCheck = null;
         this.totalClicks = 0;
@@ -111,11 +65,7 @@ class MixOrMatch {
 
             this.cardToCheck = null;
     }
-<<<<<<< HEAD
 // When Cards are Matched add matched class 
-=======
-// When Cards are Matched
->>>>>>> 8484efa (Clean code and comments for JS)
     cardMatch(card1, card2){
 this.matchedCards.push(card1);
 this.matchedCards.push(card2);
@@ -135,21 +85,13 @@ card2.classList.remove('visible');
 this.busy = false;
     }, 1000);
     }
-<<<<<<< HEAD
 // Card Identifiers for Matching from classname
-=======
-// Card Identifiers for Matching
->>>>>>> 8484efa (Clean code and comments for JS)
     getCardType(card) {
         return card.getElementsByClassName('animal-image')[0].src;
     }
 
    
-<<<<<<< HEAD
 // Game Timer (from 100 secs -> 0 secs)
-=======
-// Game Timer
->>>>>>> 8484efa (Clean code and comments for JS)
    startCountDown(){
 return setInterval(() => {
     this.timeRemaining--;
@@ -165,11 +107,7 @@ return setInterval(() => {
         document.getElementById('game-over-text').classList.add('visible');
     }
 
-<<<<<<< HEAD
     // Victory Conditions Met
-=======
-    // Victory 
->>>>>>> 8484efa (Clean code and comments for JS)
     victory() {
         clearInterval(this.countDown);
         document.getElementById('victory-text').classList.add('visible');
@@ -185,11 +123,8 @@ return setInterval(() => {
             this.cardsArray[i].style.order = randomIndex;
         }
     }
-<<<<<<< HEAD
 
     // Check if Card can be flipped - (not busy & not already in matched cards array & is not the card being checked)
-=======
->>>>>>> 8484efa (Clean code and comments for JS)
     canFlipCard(card) {
         return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
     }
@@ -201,22 +136,14 @@ function ready() {
     let cards = Array.from(document.getElementsByClassName('card'));
     let game = new LanguageGame(100, cards);
 
-<<<<<<< HEAD
 // Click to Remove Overlay (Event)
-=======
-// Click to Remove Overlay 
->>>>>>> 8484efa (Clean code and comments for JS)
     overlays.forEach(overlay => {
         overlay.addEventListener('click', () => {
             overlay.classList.remove('visible');
             game.startGame();
         });
     });
-<<<<<<< HEAD
 // Click to Flip Card (Event)
-=======
-// Click to Flip Event
->>>>>>> 8484efa (Clean code and comments for JS)
     cards.forEach(card => {
         card.addEventListener('click', () => {
             game.flipCard(card);
